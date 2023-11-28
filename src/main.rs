199643ddd30c9216bb31392d4f65ccecbe031f50
ArkhamCookie <github@mail.theuntitledproject.dev>
2023-11-28 04:07:42 -0600

    Draw paddle

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use tetra::graphics::{self, Color, Texture};
 // use tetra::input::{self, Key};
-// use tetra::math::Vec2;
+use tetra::math::Vec2;
 use tetra::{Context, ContextBuilder, State};
 
+const WINDOW_WIDTH: f32 = 640.0;
+const WINDOW_HEIGHT: f32 = 480.0;
+
 fn main() -> tetra::Result {
-	ContextBuilder::new("Pong", 640, 480)
+	ContextBuilder::new("Pong", WINDOW_WIDTH as i32, WINDOW_HEIGHT as i32)
 		.quit_on_escape(true)
 		.build()?
 		.run(GameState::new)
@@ -17,6 +20,8 @@ struct GameState {
 impl State for GameState {
 	fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
 		graphics::clear(ctx, Color::rgb(0.1, 0.5, 0.9));
+
+		self.paddle_texture.draw(ctx, Vec2::new(16.00, 16.00));
 
 		Ok(())
 	}
